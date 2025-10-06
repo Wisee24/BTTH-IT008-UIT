053e578c9ec981f66209f9c0631426364f514e48
Wisee24 <Phamduy30112006@gmail.com>
2025-10-06 15:39:54 +0700

    Update Program.cs

--- BTTH4/BTTH4/Program.cs
+++ BTTH4/BTTH4/Program.cs
@@ -1,30 +1,56 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BTTH4
+namespace BTTH3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int month, year;
-            Console.Write("Enter month/year: ");
-            month = Convert.ToInt32(Console.ReadLine());
-            year = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.Write("Enter month: ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out month))
+                {
+                    break;
+                }
+                else
+                    Console.WriteLine("Please enter a valid integer.");
+            }
+            while (true)
+            {
+                Console.Write("Enter year: ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out year))
+                {
+                    break;
+                }
+                else
+                    Console.WriteLine("Please enter a valid integer.");
+            }
             int day = FindDay(month, year);
-            if (day != -1)
-                Console.WriteLine($"There are {day} days in {month}/{year}");
-            else
-                Console.WriteLine("You did not enter a valid month or year");
+            while (day == -1)
+            {
+                Console.WriteLine("You did not enter a valid month or year, please try another month/year ");
+                Console.Write("Enter month: ");
+                month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter year: ");
+                year = Convert.ToInt32(Console.ReadLine());
+                day = FindDay(month, year);
+            }
+            Console.WriteLine($"There are {day} days in {month}/{year}");
+
         }
 
         static int FindDay(int month, int year)
         {
-            if(month < 1 || month  > 12)
+            if((month < 1 || month  > 12) || (year <= 0))
                 return -1;
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
             {
